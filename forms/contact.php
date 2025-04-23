20010a1cf5968a941187a673605c858b1e2a7360
ademnfauzi <ademnfauzi76@gmail.com>
2025-04-23 14:00:50 +0700

    update new portofolio

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,32 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  require __DIR__ . '/vendor/autoload.php';
+  use Resend\Resend;
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $receiving_email_address = 'ademnfauzi76@gmail.com';
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"]; 
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // echo "Nama: " . htmlspecialchars($name) . "<br>";
+    // echo "Email: " . htmlspecialchars($email) . "<br>";
+    // echo "Subject: " . htmlspecialchars($subject) . "<br>";
+    // echo "Message: " . htmlspecialchars($message);
 
-  echo $contact->send();
+    $resend = Resend::client('re_Ky6k7bSU_MjVDNMJKjNDicuVdjeU81iTC');
+
+    $resend->emails->send([
+      'from' => $email,
+      'to' => $receiving_email_address,
+      'subject' => $subject,
+      'html' => $message
+    ]);
+
+  } else {
+      echo "Invalid request method.";
+  }
 ?>
